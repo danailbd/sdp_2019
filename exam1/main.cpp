ca7271d7b80698001a4a788879e3b78ed7529183
Danail Dimitrov <dandimitrov@asteasolutions.com>
2019-12-24 11:22:04 +0200

    Exam.1: Introduce interpolation

--- exam1/main.cpp
+++ exam1/main.cpp
@@ -4,6 +4,12 @@
 #include <stack>
 #include "../lib/testing-framework.h"
 
+
+#define MISSING_INTERPOLATION_VAR -1
+
+const std::string LESS_VARIABLES_ERROR = "Interpolation variable missing!";
+const std::string MORE_VARIABLES_ERROR = "Additional interpolation variables!";
+
 bool isOperator(char c)
 {
     return c == '+' || c == '-' || c == '*' || c == '/';
@@ -41,18 +47,72 @@ int evaluate(std::string expression)
             operands.push(c - '0');
         } else if (isOperator(c)) {
             operators.push(c);
-        } else if (c == ')') {
+        } else if (c == ')' && operands.size() > 1) {
             int a = operands.top(); operands.pop();
             int b = operands.top(); operands.pop();
             char op = operators.top(); operators.pop();
             
             operands.push(evalSimpleExpression(a, b, op));
-
-
         }
     }
 
     return operands.top();
+}
+
+int getNextVarableIndex(std::string& expression, int startIdx)
+{
+    for (int i = startIdx; i < (int)expression.length(); ++i) {
+        if (expression[i] == '{') {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+std::string getSubstring(std::string str, int startIdx, int endIdx)
+{
+    std::string result = "";
+
+    for (int i = startIdx; i < endIdx; ++i) {
+        result += str[i];
+    }
+
+    return result;
+}
+
+std::string interpolate(std::string& expression, std::vector<int>& values, std::string& error)
+{
+    std::string interpolatedExpr = "";
+    int previousVarIdx = 0,
+        variableIdx = 0;
+
+    // while next var
+    for (auto value : values) {
+        variableIdx = getNextVarableIndex(expression, previousVarIdx);
+
+        if (variableIdx == MISSING_INTERPOLATION_VAR) {
+            error = MORE_VARIABLES_ERROR;
+            return "";
+        }
+        
+        // now omit the variable, replacing it with value
+        interpolatedExpr += getSubstring(expression, previousVarIdx, variableIdx) + std::to_string(value);
+
+        // skip the {}
+        previousVarIdx = variableIdx + 2;
+    }
+
+    // simply copy the remaining symbols
+    interpolatedExpr += getSubstring(expression, previousVarIdx, expression.length());
+
+
+    if (getNextVarableIndex(expression, previousVarIdx) != MISSING_INTERPOLATION_VAR) {
+        error = LESS_VARIABLES_ERROR;
+        return "";
+    }
+
+    return interpolatedExpr;
 }
 
 class Error
@@ -63,12 +123,12 @@ public:
 
     virtual ~Error () {};
 
-    virtual std::string getMessage() const {
+    std::string getMessage() const {
         return m_msg;
     }
 
 private:
-    size_t position;
+    std::string m_msg;
 };
 
 
@@ -124,6 +184,11 @@ void test_eval()
     int res = evaluate(populatedExpression);
 
     std::cout << res << (res == 7 ? "true" : "false");
+
+    std::string singleOperand = "(1)";
+    res = evaluate(singleOperand);
+
+    std::cout << res << (res == 1 ? "true" : "false");
 }
 
 
@@ -145,17 +210,41 @@ void test_validate()
     UnitTests::AssertEq( std::string("Operation missing at pos: 4"), validate(lackOp)[0] );
 }
 
+void test_interpolation()
+{
+    std::string simpleIterpolation = "(1 + ({} + {}))";
+    std::vector<int> values = {1, 2};
+    std::string error = "";
+
+
+    std::string interpolatedExpr = interpolate(simpleIterpolation, values, error);
+
+    UnitTests::AssertEq<std::string>( "", error, "There isn't an error" );
+    UnitTests::AssertEq<std::string>( "(1 + (1 + 2))", interpolatedExpr, "Correct expression is returned" );
+
+    std::string moreVarIntepolation = "(1 + ({} + 1))";
+    values = {1, 2};
+
+    interpolatedExpr = interpolate(moreVarIntepolation, values, error);
+
+    UnitTests::AssertEq<std::string>( MORE_VARIABLES_ERROR, error, "There is an error" );
+    UnitTests::AssertEq<std::string>( "", interpolatedExpr, "Correct expression is returned" );
+
+
+    std::string lessVarIntepolation = "(1 + ({} + {}))";
+    values = {1};
+
+    interpolatedExpr = interpolate(lessVarIntepolation, values, error);
+
+    UnitTests::AssertEq<std::string>( LESS_VARIABLES_ERROR, error, "There is an error" );
+    UnitTests::AssertEq<std::string>( "", interpolatedExpr, "Correct expression is returned" );
+}
 
 int main()
 {
-    /* test_validate(); */
-
-
-    std::vector<int> v = {1, 2, 3};
-
-    for (auto& number: v) {
-        std::cout << number << std::endl;
-    }
+    //test_validate(); 
+    //test_eval(); 
+    test_interpolation();
 
     return 0;
 }
